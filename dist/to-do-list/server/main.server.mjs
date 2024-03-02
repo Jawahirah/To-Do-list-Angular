da707eb7d316f881e34d3f464b9fee86abb2b625
Jawahirah Alsafari <Jawahirahdev@gmail.com>
2024-03-02 09:11:51 +0300

    removed the bootstrap files

--- dist/to-do-list/server/main.server.mjs
+++ dist/to-do-list/server/main.server.mjs
@@ -1,10 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  AppServerModule
-} from "./chunk-7YQINZV7.mjs";
-import "./chunk-6XQ7C7KO.mjs";
-import "./chunk-MD6NWHS6.mjs";
-export {
-  AppServerModule as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-7LYGXIJX.mjs";import"./chunk-UWUEFWCC.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
